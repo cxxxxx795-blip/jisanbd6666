f36e9b2bd9db0c72b1b28e3af5888d55bbf0b61c
cxxxxx795-blip <cxxxxx795@gmail.com>
2026-08-08 05:59:25 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,60 +1,41 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  orderBy,
-  serverTimestamp
+    getFirestore,
+    collection,
+    addDoc,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-gS6VRbVyn8njH8oLsQ6njayobuciC0",
-  authDomain: "jisandbd6666.firebaseapp.com",
-  projectId: "jisandbd6666",
-  storageBucket: "jisandbd6666.firebasestorage.app",
-  messagingSenderId: "171352567086",
-  appId: "1:171352567086:web:459bcb1f68f458d92e9ebf"
+    apiKey: "তোমার screenshot-এর API key এখানে বসাও",
+    authDomain: "jisanbd6666.firebaseapp.com",
+    projectId: "jisanbd6666",
+    storageBucket: "jisanbd6666.firebasestorage.app",
+    messagingSenderId: "171352567086",
+    appId: "1:171352567086:web:459bcb1f68f458d92e9ebf",
+    measurementId: "G-81W9M2V8YZ"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Firebase Authentication
 const auth = getAuth(app);
-
-// Firestore Database
 const db = getFirestore(app);
 
-
-// Export
 export {
-  app,
-  auth,
-  db,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  orderBy,
-  serverTimestamp
+    auth,
+    db,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    collection,
+    addDoc,
+    serverTimestamp
 };
-
-console.log("Firebase connected successfully!");
