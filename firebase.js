d1649178b8fc525b7eeea45234c9a8e157d7d85a
cxxxxx795-blip <cxxxxx795@gmail.com>
2026-08-08 06:31:47 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,30 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 import {
-  getFirestore,
-  doc,
-  setDoc,
-  collection,
-  addDoc,
-  serverTimestamp
+    getFirestore,
+    doc,
+    setDoc,
+    collection,
+    addDoc,
+    serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-gS6vRbVyfMn8jH8oLsQ6njayobuCic0",
-  authDomain: "jisanbd6666.firebaseapp.com",
-  projectId: "jisanbd6666",
-  storageBucket: "jisanbd6666.firebasestorage.app",
-  messagingSenderId: "171352567086",
-  appId: "1:171352567086:web:459bcb1f68f458d92e9ebf",
-  measurementId: "G-81W9M2V8YZ"
+    apiKey: "AIzaSyB-gS6vRbVyfMn8jH8oLsQ6njayobuCic0",
+    authDomain: "jisanbd6666.firebaseapp.com",
+    projectId: "jisanbd6666",
+    storageBucket: "jisanbd6666.firebasestorage.app",
+    messagingSenderId: "171352567086",
+    appId: "1:171352567086:web:459bcb1f68f458d92e9ebf",
+    measurementId: "G-81W9M2V8YZ"
 };
 
 
@@ -36,17 +36,17 @@ const db = getFirestore(app);
 
 
 export {
-  auth,
-  db,
+    auth,
+    db,
 
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
 
-  doc,
-  setDoc,
+    doc,
+    setDoc,
 
-  collection,
-  addDoc,
-  serverTimestamp
+    collection,
+    addDoc,
+    serverTimestamp
 };
