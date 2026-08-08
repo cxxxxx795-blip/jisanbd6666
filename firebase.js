7b9e3e24428956bcca7f460d7a0f6ad797392c2d
cxxxxx795-blip <cxxxxx795@gmail.com>
2026-08-08 07:16:52 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
-// ===============================
-// FIREBASE CONFIG
-// ===============================
+// ==================================================
+// JISANBD6666 - FIREBASE CONFIG
+// ==================================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 
 import {
     getAuth,
@@ -12,6 +15,7 @@ import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+
 import {
     getFirestore,
     collection,
@@ -20,34 +24,82 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 
+// ==================================================
+// FIREBASE CONFIG
+// ==================================================
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB-gS6vRbVyfMn8jH8oLsQ6njayobuCic0",
-    authDomain: "jisanbd6666.firebaseapp.com",
-    projectId: "jisanbd6666",
-    storageBucket: "jisanbd6666.firebasestorage.app",
-    messagingSenderId: "171352567086",
-    appId: "1:171352567086:web:459bcb1f68f458d92e9ebf",
-    measurementId: "G-81W9M2V8YZ"
+
+    apiKey:
+        "AIzaSyB-gS6vRbVyfMn8jH8oLsQ6njayobuCic0",
+
+    authDomain:
+        "jisanbd6666.firebaseapp.com",
+
+    projectId:
+        "jisanbd6666",
+
+    storageBucket:
+        "jisanbd6666.firebasestorage.app",
+
+    messagingSenderId:
+        "171352567086",
+
+    appId:
+        "1:171352567086:web:459bcb1f68f458d92e9ebf",
+
+    measurementId:
+        "G-81W9M2V8YZ"
+
 };
 
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// ==================================================
+// INITIALIZE FIREBASE
+// ==================================================
 
-const auth = getAuth(app);
+const app =
+    initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+
+// ==================================================
+// AUTH
+// ==================================================
+
+const auth =
+    getAuth(app);
 
 
-// Export
+// ==================================================
+// FIRESTORE
+// ==================================================
+
+const db =
+    getFirestore(app);
+
+
+// ==================================================
+// EXPORT
+// ==================================================
+
 export {
+
     auth,
+
     db,
+
     createUserWithEmailAndPassword,
+
     signInWithEmailAndPassword,
+
     signOut,
+
     onAuthStateChanged,
+
     collection,
+
     addDoc,
+
     serverTimestamp
+
 };
